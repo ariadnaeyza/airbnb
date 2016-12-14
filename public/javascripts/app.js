3c9da73427f08d9c337b771f490d144fd66b6a5b
ANDREA <andrea.palomino.ruby@gmail.com>
2016-12-14 09:30:02 -0500

    add marker html

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,193 +1,111 @@
-// function initMap() {
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 6,
-//   });
-//   var infoWindow = new google.maps.InfoWindow({map: map});
+var cargarPagina = function(){
+  calendario();
+};
 
-//   // Try HTML5 geolocation.
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//       var pos = {
-//         lat: position.coords.latitude,
-//         lng: position.coords.longitude
-//       };
-
-//       infoWindow.setPosition(pos);
-//       infoWindow.setContent('Location found.');
-//       map.setCenter(pos);
-//     }, function() {
-//       handleLocationError(true, infoWindow, map.getCenter());
-//     });
-//   } else {
-//     // Browser doesn't support Geolocation
-//     handleLocationError(false, infoWindow, map.getCenter());
-//   }
-// }
-
-
-// function cargarMapa(position) {
-//   var lat = position.coords.latitude; // Guardamos nuestra latitud
-//     var lon = position.coords.longitude; // Guardamos nuestra longitud
-//     var latlon = new google.maps.LatLng(lat, lon); // Creamos un punto con nuestras coordenadas
-//   $("#mapa").addClass("classMap");
-
-//     var propiedadMapa = {
-//       center: latlon, // Definimos la posicion del mapa con el punto
-//       zoom: 16,
-//       mapTypeId: google.maps.MapTypeId.ROADMAP,
-//       mapTypeControl: false,
-//       disableDefaultUI: true,
-//       streetViewControl: false,
-//       zoomControl: false
-//     };
-//     // Creamos el mapa y lo situamos en su capa
-//     map = new google.maps.Map(document.getElementById('mapa'), propiedadMapa);
-//     // Creamos el objeto principal para realizar la petición de consulta a Google Maps
-//     geocoder = new google.maps.Geocoder();
-//     // Marcamos nuestra ubicación
-//   miUbicacion();
-
-//  };
-
-// function miUbicacion() {
-//   var latlon = map.getCenter(); // Obtener la posición del mapa
-//     var propiedadMarker = {
-//       map: map, // Vinculamos al mapa
-//       position: latlon, // Nos situamos en nuestro punto
-//       draggable: true, // Nos permite poder mover el marcador
-//       icon: 'img/marker.png'
-//     };
-
-//     // Creamos un marcador en el mapa
-//     markerInicio = new google.maps.Marker(propiedadMarker);
-//     // Cada vez que insertemos un marcador, la insertamos en el array mediante el método .push()
-//     markers.push(markerInicio);
-//     // Agregamos la dirección en el input (Punto de Partida)
-//   agregarDireccionInicio();
-//   // Agregamos la function a ejecutar al evento de mover el marcador
-//     google.maps.event.addListener(markerInicio, 'position_changed', agregarDireccionInicio);
-// };
-
-// function geolocate() {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(function(position) {
-//       var geolocation = {
-//         lat: position.coords.latitude,
-//         lng: position.coords.longitude
-//       };
-//       var circle = new google.maps.Circle({
-//         center: geolocation,
-//         radius: position.coords.accuracy
-//       });
-//       autocomplete.setBounds(circle.getBounds());
-//       });
-//   }
-// };
-
-// var sigt = function(e){
-// 	e.preventDefault();
-// 	var distritoIngresado = $('#icon_prefix').val();  
-// 		$.ajax({ 
-// 		  url: "/sitios?lugar="+distritoIngresado,
-// 		  type: "GET",
-// 		  success: function(response) {
-// 		  	console.log(response);
-// 		  } 
-// 		});
-// }
-$(document).ready(function(){
+$(document).ready(cargarPagina);
      // $("#btn").click(sigt);
 
-    $(function() {
-    var dateFormat = "mm/dd/yy",
-		from = $("#from")
-		.datepicker({
-		defaultDate: "+1w",
-		changeMonth: true,
-		changeYear: true,
-		minDate: '10/10/2016',
-		maxDate: '5/5/2020',
-		numberOfMonths: 1
-    })
+var directionsService = new google.maps.DirectionsService();
+var directionsRenderer = new google.maps.DirectionsRenderer();
+var map=$("#mapa");
+var marker;
+var lat;
+var long;
+
+var calendario = function() {
+  var dateFormat = "mm/dd/yy",
+    from = $("#from")
+    .datepicker({
+    defaultDate: "+1w",
+    changeMonth: true,
+    changeYear: true,
+    minDate: '10/10/2016',
+    maxDate: '5/5/2020',
+    numberOfMonths: 1
+  })
     .on("change", function() {
        to.datepicker("option", "minDate", getDate(this));
     }),
-	to = $("#to").datepicker({
-		defaultDate: "+1w",
-		changeMonth: true,
-		changeYear: true,
-		minDate: '10/10/2016',
-		maxDate: '5/5/2020',
-		numberOfMonths: 1
-	})
-	.on("change", function() {
-		from.datepicker("option", "maxDate", getDate(this));
-	});
-	function getDate(element) {
-	var date;
-		try {
-		date = $.datepicker.parseDate(dateFormat, element.value);
-		} catch (error) {
-		date = null;
-		}
-		return date;
-		}
-		$("#showTo").click(function() {
-			$("#from").datepicker("show");
-	   });
+    to = $("#to").datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      changeYear: true,
+      minDate: '10/10/2016',
+      maxDate: '5/5/2020',
+      numberOfMonths: 1
+    })
+    .on("change", function() {
+      from.datepicker("option", "maxDate", getDate(this));
     });
-	$( "#slider-range" ).slider({
-		range: true,
-		min: 0,
-		max: 500,
-		values: [ 75, 300 ],
-		slide: function( event, ui ) {
-		$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-		}
-	});
+  function getDate(element) {
+  var date;
+    try {
+    date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch (error) {
+    date = null;
+    }
+    return date;
+    }
+    $("#showTo").click(function() {
+      $("#from").datepicker("show");
+    });
+  };
+
+$( "#slider-range" ).slider({
+  range: true,
+  min: 0,
+  max: 500,
+  values: [ 75, 300 ],
+  slide: function( event, ui ) {
+  $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+  }
+});
     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
       " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-    var availableTags = [
-      "Cercado de Lima",
-      "Bellavista",
-      "Carmen de la Legua",
-      "La Perla",
-      "La Punta",
-      "Ventanilla",
-      "Callao",
-      "Ate",
-      "Barranco",
-      "Breña",
-      "Comas",
-      "Chorrillos",
-      "El Agustino",
-      "Jesús María",
-      "La Molina",
-      "La Victoria",
-      "Lince",
-      "Magdalena",
-      "Miraflores",
-      "Pueblo Libre",
-      "Puente Piedra",
-      "Rimac",
-      "San Isidro",
-      "Independencia",
-      "San Juan de Miraflores",
-      "San Luis",
-      "San Martín de Porres",
-      "San Miguel",
-      "antiago de Surco",
-      "Surquillo",
-      "Villa María del Triunfo",
-      "San Juan de Lurigancho",
-      "Santa Rosa",
-      "Los Olivos",
-      "San Borja",
-      "Villa el Salvador",
-      "Santa Anita"
-    ];
-    $( "#tags" ).autocomplete({
-      source: availableTags
+
+var mostrarMapa =function(){
+ 
+    var input = $("#buscador1");
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.addListener('place_changed', function() {
+        directionsRenderer.setMap(null);
+        
+        if (marker != null) {
+            marker.setMap(null);    
+        }
+        
+        marker = new google.maps.Marker({
+            map: map,
+            anchorPoint: new google.maps.Point(0, -29)
+        });
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+          window.alert("Autocomplete's returned place contains no geometry");
+          return;
+        }
+
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(17);
+        }
+        marker.setIcon(({
+          url: '../img/map-pin-blue.png'
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode( { "address": $input.val()}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                lat = results[0].geometry.location.lat();
+                long = results[0].geometry.location.lng();
+            } 
+        });
+
     });
-});
+};
+google.maps.event.addDomListener(window, 'load', mostrarMapa);

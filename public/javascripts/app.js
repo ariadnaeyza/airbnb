836e73b54d6bc31295c851dda4c05e6bdde8a8d2
Paola Ortiz de Zevallos <paolaozv@gmail.com>
2016-12-14 09:26:35 -0500

    mapa

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,3 @@
-
 var cargarPagina = function() {
   /*$("#btn").click(sigt);*/
   autocompletar();
@@ -49,51 +48,51 @@ var autocompletar = function() {
      
     $(function() {
     var dateFormat = "mm/dd/yy",
-		from = $("#from")
-		.datepicker({
-		defaultDate: "+1w",
-		changeMonth: true,
-		changeYear: true,
-		minDate: '10/10/2016',
-		maxDate: '5/5/2020',
-		numberOfMonths: 1
+    from = $("#from")
+    .datepicker({
+    defaultDate: "+1w",
+    changeMonth: true,
+    changeYear: true,
+    minDate: '10/10/2016',
+    maxDate: '5/5/2020',
+    numberOfMonths: 1
     })
     .on("change", function() {
        to.datepicker("option", "minDate", getDate(this));
     }),
-	to = $("#to").datepicker({
-		defaultDate: "+1w",
-		changeMonth: true,
-		changeYear: true,
-		minDate: '10/10/2016',
-		maxDate: '5/5/2020',
-		numberOfMonths: 1
-	})
-	.on("change", function() {
-		from.datepicker("option", "maxDate", getDate(this));
-	});
-	function getDate(element) {
-	var date;
-		try {
-		date = $.datepicker.parseDate(dateFormat, element.value);
-		} catch (error) {
-		date = null;
-		}
-		return date;
-		}
-		$("#showTo").click(function() {
-			$("#from").datepicker("show");
-	   });
+  to = $("#to").datepicker({
+    defaultDate: "+1w",
+    changeMonth: true,
+    changeYear: true,
+    minDate: '10/10/2016',
+    maxDate: '5/5/2020',
+    numberOfMonths: 1
+  })
+  .on("change", function() {
+    from.datepicker("option", "maxDate", getDate(this));
+  });
+  function getDate(element) {
+  var date;
+    try {
+    date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch (error) {
+    date = null;
+    }
+    return date;
+    }
+    $("#showTo").click(function() {
+      $("#from").datepicker("show");
+     });
     });
-	$( "#slider-range" ).slider({
-		range: true,
-		min: 0,
-		max: 500,
-		values: [ 75, 300 ],
-		slide: function( event, ui ) {
-		$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-		}
-	});
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [ 75, 300 ],
+    slide: function( event, ui ) {
+    $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
       " - $" + $( "#slider-range" ).slider( "values", 1 ) );
     var availableTags = [
